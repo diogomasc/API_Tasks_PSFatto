7352d9c39269fa3cf0bd839bf19c39513c288372
Diogo Mascarenhas <diogomascarenhas0574@gmail.com>
2024-11-10 23:53:49 -0300

    Feat: remoção do  teste inicial do Prisma na index.js

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 import express from "express";
 import cors from "cors";
 import taskRoutes from "./routes/tasks.js";
-import { PrismaClient } from "@prisma/client";
 
 // Configurações do app
 const app = express();
-const prisma = new PrismaClient();
 
 // Middleware
 app.use(cors());
@@ -19,29 +17,9 @@ app.use((req, res) => {
   res.status(404).json({ message: "Rota não encontrada" });
 });
 
-// Teste inicial do Prisma
-async function testarConexao() {
-  try {
-    const novaTarefa = await prisma.task.create({
-      data: {
-        displayOrder: 1,
-        description: "Primeira tarefa de teste",
-        value: 150.0,
-        deadline: new Date("2024-12-31"),
-      },
-    });
-    console.log("Tarefa criada com sucesso:", novaTarefa);
-  } catch (erro) {
-    console.error("Erro ao criar tarefa:", erro);
-  } finally {
-    await prisma.$disconnect();
-  }
-}
-
-// Inicialização do servidor e teste do Prisma
+// Inicialização do servidor
 app.listen(8800, () => {
   console.log("Servidor rodando em: http://localhost:8800");
-  testarConexao();
 });
 
 // Tratamento de erros não capturados
